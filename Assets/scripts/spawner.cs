ebab8264e944cb0549d26ee65d66e59f34ff5a50
Sebatian Pindral <101107955+Majonezkielecki@users.noreply.github.com>
2025-05-05 17:06:22 +0200

    Update spawner.cs

--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -6,29 +6,27 @@ public class BiomeSpawnerByHeight : MonoBehaviour
     public GameObject treePrefab;
     public GameObject rockPrefab;
     public GameObject sandPrefab;
-    public int numberOfSpawns = 100;
+    private int numObject = 100;
 
     void Start()
     {
         TerrainData terrainData = terrain.terrainData;
 
-        for (int i = 0; i < numberOfSpawns; i++)
+        for (int i = 0; i < numObject; i++)
         {
-            float terrainWidth = terrainData.size.x;
-            float terrainLength = terrainData.size.z;
+            float Width = terrainData.size.x;
+            float Length = terrainData.size.z;
 
-            float x = Random.Range(0, terrainWidth);
-            float z = Random.Range(0, terrainLength);
+            float x = Random.Range(0, Width);
+            float z = Random.Range(0, Length);
 
-            Vector3 worldPos = new Vector3(x, 0, z) + terrain.transform.position;
-            float height = terrain.SampleHeight(worldPos) + terrain.transform.position.y;
+            float height = terrain.SampleHeight(
+                new Vector3(x, 0, z) + terrain.transform.position) + terrain.transform.position.y;
 
             GameObject prefab = GetPrefabByHeight(height);
-            if (prefab != null)
-            {
-                Vector3 spawnPos = new Vector3(x, height, z) + terrain.transform.position;
-                Instantiate(prefab, spawnPos, Quaternion.identity);
-            }
+            Vector3 spawnPos = new Vector3(x, height, z) + terrain.transform.position;
+            Instantiate(prefab, spawnPos, Quaternion.identity);
+
         }
     }
 
